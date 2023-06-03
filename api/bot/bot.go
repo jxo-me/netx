2f79c83a16ed3404f7de2a3ef99894052c0f4183
Monkey <mr.monkey.job@gmail.com>
2023-06-03 18:36:58 +0800

    update bot api

--- api/bot/bot.go
+++ api/bot/bot.go
@@ -8,6 +8,24 @@ var (
 			telebot.OnText:       Event.OnText,
 			telebot.OnCallback:   Event.OnCallback,
 			telebot.OnUserJoined: Event.OnUserJoined,
+			// Click Callback
+			OnClickAdmissions:  Event.OnClickService,
+			OnClickAuthers:     Event.OnClickService,
+			OnClickBypass:      Event.OnClickService,
+			OnClickHops:        Event.OnClickService,
+			OnClickIngress:     Event.OnClickService,
+			OnClickServices:    Event.OnClickService,
+			OnClickChains:      Event.OnClickService,
+			OnClickHosts:       Event.OnClickService,
+			OnClickResolver:    Event.OnClickService,
+			OnClickLimiter:     Event.OnClickService,
+			OnClickConnLimiter: Event.OnClickService,
+			OnClickRateLimiter: Event.OnClickService,
+			OnClickConfig:      Event.OnClickService,
+			// back services
+			OnBackServices: Event.OnBackServices,
+			// TextCommand
+			HostTextCommand: Event.OnHostTextCommand,
 		},
 		Btns: map[*telebot.Btn]telebot.HandlerFunc{
 			//&bot.BtnBetting:       bot.Event.OnBtnBetting,
--- api/bot/event.go
+++ api/bot/event.go
@@ -1,9 +1,29 @@
 package bot
 
-import telebot "github.com/jxo-me/gfbot"
+import (
+	"fmt"
+	telebot "github.com/jxo-me/gfbot"
+)
 
 var (
-	Event = hEvent{}
+	Event           = hEvent{}
+	HostTextCommand = "/myHosts"
+	// Click group
+	OnClickAdmissions  = "\fAdmissions"
+	OnClickAuthers     = "\fAuthers"
+	OnClickBypass      = "\fBypass"
+	OnClickHops        = "\fHops"
+	OnClickIngress     = "\fIngress"
+	OnClickServices    = "\fServices"
+	OnClickChains      = "\fChains"
+	OnClickHosts       = "\fHosts"
+	OnClickResolver    = "\fResolver"
+	OnClickLimiter     = "\fLimiter"
+	OnClickConnLimiter = "\fConnLimiter"
+	OnClickRateLimiter = "\fRateLimiter"
+	OnClickConfig      = "\fConfig"
+
+	OnBackServices = "\fbackServices"
 )
 
 type (
@@ -12,6 +32,62 @@ type (
 
 func (h *hEvent) OnText(c telebot.Context) error {
 	return c.Send("OnText")
+}
+
+func getSelectMenus() *telebot.ReplyMarkup {
+	selector := &telebot.ReplyMarkup{}
+	selector.Inline(
+		selector.Row(
+			selector.Data("@Admissions", "Admissions", "Admissions"),
+			selector.Data("@Authers", "Authers", "Authers"),
+			selector.Data("@Bypass", "Bypass", "Bypass"),
+		),
+		selector.Row(
+			selector.Data("@Hops", "Hops", "Hops"),
+			selector.Data("@Ingress", "Ingress", "Ingress"),
+			selector.Data("@Services", "Services", "Services"),
+		),
+		selector.Row(
+			selector.Data("@Chains", "Chains", "Chains"),
+			selector.Data("@Hosts", "Hosts", "Hosts"),
+			selector.Data("@Resolver", "Resolver", "Resolver"),
+		),
+		selector.Row(
+			selector.Data("@Limiter", "Limiter", "Limiter"),
+			selector.Data("@ConnLimiter", "ConnLimiter", "ConnLimiter"),
+			selector.Data("@RateLimiter", "RateLimiter", "RateLimiter"),
+		),
+		selector.Row(
+			selector.Data("@Config", "Config", "Config"),
+		),
+	)
+	return selector
+}
+
+func (h *hEvent) OnBackServices(c telebot.Context) error {
+	selector := getSelectMenus()
+	return c.Edit("从下面的列表中选择一个服务:", selector)
+}
+
+func (h *hEvent) OnHostTextCommand(c telebot.Context) error {
+	selector := getSelectMenus()
+	return c.Send("从下面的列表中选择一个服务:", selector)
+}
+
+func (h *hEvent) OnClickService(c telebot.Context) error {
+	user := c.Callback().Sender
+	cmd := c.Callback().Data
+	msg := fmt.Sprintf("选中服务: %s %d.\nWhat do you want to do with the bot?", cmd, user.ID)
+	selector := &telebot.ReplyMarkup{}
+	selector.Inline(
+		selector.Row(
+			selector.Data("@update", "update", "update"),
+			selector.Data("@delete", "delete", "delete"),
+			selector.Data("@list", "list", "list"),
+		),
+		selector.Row(selector.Data("« 返回服务列表", "backServices", "backServices")),
+	)
+	return c.Edit(msg, selector)
 }
 
 func (h *hEvent) OnCallback(c telebot.Context) error {
